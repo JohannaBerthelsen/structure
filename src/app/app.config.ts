afe2c704325fa7154cf76e069f77f98915b2bcb1
JohannaBerthelsen <johannalberthelsen@gmail.com>
2026-05-17 21:47:46 +0200

    add ,

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,23 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideKirby, withGlobalSetup } from '@kirbydesign/designsystem';
+import {
+  BUILT_IN_ICONS_URL,
+  provideKirby,
+  withGlobalSetup,
+} from '@kirbydesign/designsystem';
+import { initializeParse } from './parse-config';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideKirby(withGlobalSetup())
-  ]
+    provideKirby(withGlobalSetup()),
+  ],
 };
