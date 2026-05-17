1e720e813458cf91b677d06a985aca4e118a66da
JohannaBerthelsen <johannalberthelsen@gmail.com>
2026-05-17 21:47:35 +0200

    bug: remove parse from this file to prevent blank page

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { App } from './app/app';
-import { initializeParse } from './app/parse-config';
+import { appConfig } from './app/app.config';
 
-initializeParse();
-
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(App, appConfig).catch((err) => console.error(err));
