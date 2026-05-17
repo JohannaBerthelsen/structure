2c7bb0761f01729af19f43d5b45fce178315c862
JohannaBerthelsen <johannalberthelsen@gmail.com>
2026-05-17 21:48:21 +0200

    prettier

--- src/app/todo-list/todo-list.ts
+++ src/app/todo-list/todo-list.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
-import { CardComponent, CheckboxComponent, ItemComponent } from "@kirbydesign/designsystem";
+import {
+  CardComponent,
+  CheckboxComponent,
+  ItemComponent,
+} from '@kirbydesign/designsystem';
+import { IconModule } from '@kirbydesign/designsystem/icon';
 
 @Component({
   selector: 'app-todo-list',
-  imports: [ItemComponent, CheckboxComponent, CardComponent],
+  imports: [ItemComponent, CheckboxComponent, CardComponent, IconModule],
   templateUrl: './todo-list.html',
   styleUrls: ['./todo-list.scss'],
 })
-export class TodoList {
-
-}
+export class TodoList {}
